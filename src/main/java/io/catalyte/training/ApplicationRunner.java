07aa78a30d34f43b938656a05874a1043f4e3fdd
Nicolas Hemenway <nhemenway@catalystsolves.com>
2020-09-18 02:02:14 -0400

    forgot to add jdoc to ApplicationRunner

--- src/main/java/io/catalyte/training/ApplicationRunner.java
+++ src/main/java/io/catalyte/training/ApplicationRunner.java
@@ -3,6 +3,9 @@ package io.catalyte.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Starts the application
+ */
 @SpringBootApplication
 public class ApplicationRunner {
 
